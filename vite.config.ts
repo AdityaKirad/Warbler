3875cb4ea5a5e4c71e9803ca1da2b21b3bcd0555
Aditya <kiradaditya2001@gmail.com>
2025-03-08 16:54:36 +0530

    shadcn setuped

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,34 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { remixDevTools } from "remix-development-tools";
+import { flatRoutes } from "remix-flat-routes";
 
 declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
+	interface Future {
+		v3_singleFetch: true;
+	}
 }
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+	plugins: [
+		remixDevTools({
+			suppressDeprecationWarning: true,
+		}),
+		remix({
+			future: {
+				v3_fetcherPersist: true,
+				v3_relativeSplatPath: true,
+				v3_throwAbortReason: true,
+				v3_singleFetch: true,
+				v3_lazyRouteDiscovery: true,
+			},
+			ignoredRouteFiles: ["**/*"],
+			routes: async (defineRoutes) =>
+				flatRoutes("routes", defineRoutes, {
+					ignoredRouteFiles: ["**/*.server.*", "**/*.client.*"],
+				}),
+		}),
+		tsconfigPaths(),
+	],
 });
