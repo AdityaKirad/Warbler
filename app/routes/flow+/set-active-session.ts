acb4a8e62b3c5ecd140b1fa4354c6ccca31fc33b
Aditya <kiradaditya2001@gmail.com>
2026-05-07 09:42:52 +0530

    feat: enhance session management with session data storage and expiration handling

--- app/routes/flow+/set-active-session.ts
+++ app/routes/flow+/set-active-session.ts
@@ -1,3 +1,4 @@
+import { getExpirationDate } from "~/.server/authentication";
 import {
   createMultiSessionCookieId,
   MULTI_SESSION_COOKIE_PREFIX,
@@ -5,12 +6,15 @@ import {
   sessionCookieOptions,
 } from "~/.server/cookies/session";
 import { db } from "~/.server/drizzle";
+import { sessionDataStorage } from "~/.server/session/session-data";
 import { createCookie, redirect, redirectDocument } from "react-router";
 import type { Route } from "./+types/set-active-session";
 
 export async function action({ request }: Route.ActionArgs) {
   const url = new URL(request.url);
   const token = url.searchParams.get("token");
+
+  const cookie = request.headers.get("cookie");
 
   if (!token) {
     throw redirect("/login");
@@ -21,12 +25,24 @@ export async function action({ request }: Route.ActionArgs) {
     sessionCookieOptions,
   );
 
-  if (!(await multiSessionCookie.parse(request.headers.get("cookie")))) {
+  if (!(await multiSessionCookie.parse(cookie))) {
     throw redirect("/login");
   }
 
   const session = await db.query.session.findFirst({
-    columns: { token: true, expiresAt: true },
+    columns: { token: true, updatedAt: true, expiresAt: true },
+    with: {
+      user: {
+        columns: {
+          id: true,
+          name: true,
+          username: true,
+          photo: true,
+          profileVerified: true,
+          onboardingStepsCompleted: true,
+        },
+      },
+    },
     where: (session, { and, eq, gt }) =>
       and(eq(session.token, token), gt(session.expiresAt, new Date())),
   });
@@ -38,6 +54,20 @@ export async function action({ request }: Route.ActionArgs) {
       },
     });
   }
+
+  const sessionData = await sessionDataStorage.getSession(cookie);
+
+  const sessionDataExpiresAt = getExpirationDate(5 * 60);
+
+  sessionData.set("session", {
+    session: {
+      updatedAt: session.updatedAt.getTime(),
+      expiresAt: session.expiresAt.getTime(),
+    },
+    user: session.user,
+  });
+  sessionData.set("updatedAt", Date.now());
+  sessionData.set("expiresAt", sessionDataExpiresAt.getTime());
 
   const referer = request.headers.get("referer");
 
@@ -53,6 +83,12 @@ export async function action({ request }: Route.ActionArgs) {
     "set-cookie",
     await multiSessionCookie.serialize(session.token, {
       expires: session.expiresAt,
+    }),
+  );
+  headers.append(
+    "set-cookie",
+    await sessionDataStorage.commitSession(sessionData, {
+      expires: sessionDataExpiresAt,
     }),
   );
 
